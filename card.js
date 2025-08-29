81fce645b92e5e855a61e22c58a620124d990432
Piyush <sagarwal2k20@gmail.com>
2025-08-29 15:26:03 +0530

    Update card.js

--- card.js
+++ card.js
@@ -60,18 +60,3 @@ const btn =
   btn.style.backgroundColor = "#ff6f61";
   btn.style.color = "white";
 }
-function checkLogin() {
-  if (localStorage.getItem("isLoggedIn") !== "true") {
-    window.location.replace("index.html");
-  }
-}
-
-// Run on first load
-window.addEventListener("load", checkLogin);
-
-// Run when page is restored from cache (back/forward button)
-window.addEventListener("pageshow", function (event) {
-  if (event.persisted) {
-    checkLogin();
-  }
-});
